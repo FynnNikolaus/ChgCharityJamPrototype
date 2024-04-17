138b12b7f5908ae3dee139b69156da1e23b2587e
Alder, Joachim <Joachim.Alder@chg-meridian.com>
2024-04-17 12:48:56 +0200

    add views & Controller actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,16 @@ namespace ChgCharityJamPrototype.Controllers
 		{
 			return View();
 		}
+		
+		public IActionResult Board()
+		{
+			return View();
+		}
+		
+		public IActionResult BoardBackend()
+		{
+			return View();
+		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
