d4799de7906a38041422b2007233cc47708c2516
Spiller, Pascal <Pascal.Spiller@chg-meridian.com>
2024-04-17 16:42:25 +0200

    Add Card model.

--- Controllers/BackendBoardControllers.cs
+++ Controllers/BackendBoardControllers.cs
@@ -1,27 +1,35 @@
 using ChgCharityJamPrototype.Models;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
 
 namespace ChgCharityJamPrototype.Controllers
 {
-	public class BackendBoardController : Controller
-	{
-		private readonly ILogger<BackendBoardController> _logger;
+    public class BackendBoardController : Controller
+    {
+        private readonly ILogger<BackendBoardController> _logger;
 
-		public BackendBoardController(ILogger<BackendBoardController> logger)
-		{
-			_logger = logger;
-		}
+        public BackendBoardController(ILogger<BackendBoardController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            var cardModel = new CardModel();
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "bin", "debug", "net8.0", "wwwroot", "Assets", "DE", "Cards.json");
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                var jsonObject = JsonConvert.DeserializeObject<List<Card>>(sr.ReadToEnd());
+
+                if (jsonObject != null)
+                    cardModel.AddCards((IEnumerable<Card>)jsonObject);
+            }
+
+            return View(cardModel);
+        }
+    }
 }
---
+++ Models/Card.cs
@@ -0,0 +1,27 @@
+﻿namespace ChgCharityJamPrototype.Models
+{
+    public class Card
+    {
+        public Card(ActivationMode activationMode, string id, string name, string text, int? effectDurationInMinutes)
+        {
+            ActivationMode = activationMode;
+            Id = id;
+            Name = name;
+            Text = text;
+            EffectDurationInMinutes = effectDurationInMinutes;
+        }
+
+        public ActivationMode ActivationMode { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public int? EffectDurationInMinutes { get; set; }
+    }
+
+    public enum ActivationMode
+    {
+        Public,
+        Anonymous,
+        Secret
+    }
+}
