e2763946745fda0677ecaea7f61c0684269ea43c
Alder, Joachim <Joachim.Alder@chg-meridian.com>
2024-04-17 15:54:37 +0200

    add jquery, add grid to fronted Board

--- wwwroot/js/channel.js
+++ wwwroot/js/channel.js
@@ -1,39 +1,24 @@
 "use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/communicationHub").build();
+$(document).ready(function () {
+    var connection = new signalR.HubConnectionBuilder().withUrl("/communicationHub").build();
 
-//Disable the send button until connection is established.
-document.getElementById("sendButton").disabled = true;
-
-connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
-});
-
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    connection.start().then(function () {
+        console.log('Signal Run');
+    }).catch(function (err) {
         return console.error(err.toString());
     });
-    event.preventDefault();
-});
 
-function sendCard() {
-    var card = document.getElementById("cards").value;
-    var team = "deine Mama";
+    function sendCard() {
+        var card = $("#cards").val();
+        var team = "deine Mama";
 
-    connection.invoke("PlayCard", card, team).catch(function (err) {
-        return console.error(err.toString());
+        connection.invoke("PlayCard", card, team).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+
+    $(".sendCard").click(function () {
+        sendCard();
     });
-}
+});
--- wwwroot/js/recipient.js
+++ wwwroot/js/recipient.js
@@ -1,27 +1,16 @@
 "use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/communicationHub").build();
+$(document).ready(function () {
+    var connection = new signalR.HubConnectionBuilder().withUrl("/communicationHub").build();
 
-connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
-}); 
+    connection.on("ReceiveCard", function (card, team) {
+        var li = $("<li></li>").text(`${team} played ${card}`);
+        $("#messagesList").append(li);
+    });
 
-connection.on("ReceiveCard", function (card, team) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${team} played ${card}`;
-});
-
-connection.start().then(function () {
-    console.log("SignalR UP");
-}).catch(function (err) {
-    return console.error(err.toString());
+    connection.start().then(function () {
+        console.log("SignalR UP");
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
 });
