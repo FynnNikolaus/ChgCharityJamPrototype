ba69c9329562b1f5fcd0140c7d5cda6dea9bc726
Nikolaus, Fynn <fynn.nikolaus@yahoo.com>
2024-05-10 19:10:23 +0200

    Add board.js reciver

---
+++ wwwroot/js/user_panel/board.js
@@ -0,0 +1,22 @@
+﻿var connection = new signalR.HubConnectionBuilder()
+	.withUrl("/communicationHub")
+	.build();
+
+connection.on("UpdateGameStatus", function (gameStatus) {
+	console.log("Spielstatus aktualisiert:", gameStatus);
+	updateUI(gameStatus);
+});
+
+connection.start().then(function () {
+	console.log("Verbunden mit SignalR.");
+}).catch(function (err) {
+	console.error(err.toString());
+});
+
+function updateUI(gameStatus) {
+	$("#game-status").empty();
+	gameStatus.Teams.forEach(function (team) {
+		$("#game-status").append("<div>Team " + team.Id + ": " + team.Name + "</div>");
+	});
+	$("#uptime").text("Betriebszeit: " + gameStatus.Uptime);
+}
